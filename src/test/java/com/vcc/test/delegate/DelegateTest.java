aef9556840c78e84e65e40b91ebbaea72f4ef738
vcralete <1041497818@qq.com>
2019-12-28 14:23:34 +0800

    init

---
+++ src/test/java/com/vcc/test/delegate/DelegateTest.java
@@ -0,0 +1,15 @@
+package com.vcc.test.delegate;
+
+
+import org.testng.annotations.Test;
+
+public class DelegateTest {
+    
+    @Test
+    public void Test(){
+        System.out.println("hello world");
+    }
+    
+    
+    
+}
